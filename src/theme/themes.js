67a86257296abcf93237172e9b35a3c44c7d558c
AToner25 <70965075+AToner25@users.noreply.github.com>
2024-02-06 09:57:06 -0500

    Add files via upload

---
+++ src/theme/themes.js
@@ -0,0 +1,45 @@
+export const lightTheme = {
+  background: 'white',
+  color: '#121212',
+  accentColor: '#3D84C6',
+  chronoTheme: {
+    cardBgColor: 'white',
+    cardForeColor: 'black',
+    titleColor: 'white',
+  },
+  timelineLineColor: '#ccc',
+  cardBackground: '#fff',
+  cardFooterBackground: 'black',
+  cardBorderColor: '#00000020',
+  navbarTheme: {
+    linkColor: '#dedede',
+    linkHoverColor: '#fefefe',
+    linkActiveColor: '#fefefe',
+  },
+  bsPrimaryVariant: 'light',
+  bsSecondaryVariant: 'dark',
+  socialIconBgColor: '#121212',
+};
+
+export const darkTheme = {
+  background: 'white',
+  color: 'black',
+  accentColor: 'black',
+  chronoTheme: {
+    cardBgColor: '#fff',
+    cardForeColor: 'black',
+    titleColor: 'black',
+  },
+  timelineLineColor: 'red',
+  cardBackground: '#060606',
+  cardFooterBackground: '#181818',
+  cardBorderColor: '#ffffff20',
+  navbarTheme: {
+    linkColor: 'black',
+    linkHoverColor: 'black',
+    linkActiveColor: 'black',
+  },
+  bsPrimaryVariant: 'dark',
+  bsSecondaryVariant: 'light',
+  socialIconBgColor: 'black',
+};
